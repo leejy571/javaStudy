f211d242635251f9a7e01ceb04a64df811e1410d
Jae Yong Lee <jaeyonglee571@gmail.com>
2017-05-31 21:38:14 +0900

    201705312138

--- src/hwArrayTestMultiplyOne.java
+++ src/hwArrayTestMultiplyOne.java
@@ -3,17 +3,17 @@
  */
 public class hwArrayTestMultiplyOne {
     public static void main(String[] args) {
-        int[][] arrayOne = {{0, 1, 2, 3, 4},
-                {5, 6, 7, 8, 9},
-                {10, 11, 12, 13, 14}
+        int[][] arrayOne = {{0, 1, 2},
+                {5, 6, 7},
+                {10, 11, 12}
         };
-        int[][] arrayTwo = {{2, 5, 2, 9, 1},
-                {8, 4, 2, 0, 5},
-                {9, 100, 88, 40, 0},
+        int[][] arrayTwo = {{2, 5, 2},
+                {8, 4, 2},
+                {9, 100, 88},
         };
 
         int sum = 0;
-        int[][] arrayThree = new int[3][5];
+        int[][] arrayThree = new int[3][3];
 
         for (int row = 0; row < arrayThree.length; row++) {
             for (int column = 0; column < arrayThree[0].length; column++) {
