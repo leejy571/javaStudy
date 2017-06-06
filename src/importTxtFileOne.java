0b28941fb2469f0c1cbcfa7715fb9637c3eea05d
Jae Yong Lee <jaeyonglee571@gmail.com>
2017-06-06 22:32:50 +0900

    201706062332

--- src/importTxtFileOne.java
+++ src/importTxtFileOne.java
@@ -8,25 +8,21 @@ public class importTxtFileOne {
    public static void main (String[] args) {
        String fileName = "/home/cad113/Desktop/arrayOne";
        String line = null;
-
-       try {
+       try
+       {
            FileReader fileReader = new FileReader(fileName);
            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
            while ((line = bufferedReader.readLine()) != null) {
                System.out.println(line);
            }
            bufferedReader.close();
        }
-
        catch (FileNotFoundException ex) {
            System.out.println("The file named " + fileName + ".");
        }
        catch (IOException ex) {
            System.out.println("File hmmmm");
        }
-
-
 
    }
 }
